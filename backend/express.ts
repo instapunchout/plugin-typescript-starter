2bfa34d957b5a515d92afddd491217e86d12a7b4
Oussama Gammoudi <md.oussama@gmail.com>
2023-02-07 18:55:06 +0100

    add proxy and order apis

--- backend/express.ts
+++ backend/express.ts
@@ -12,7 +12,20 @@ export interface ProxyResponse {
   email: string;
 }
 
-app.get('/punchout/login', async (req, res) => {
+// will be called by the procurement system
+// proxies all requests to punchout.cloud
+app.get('/punchout/cxml/:id', async (req, res) => {
+  // the req.body is an xml string
+  let response = await axios
+    .post("https://punchout.cloud/cxml/" + req.params.id, req.body);
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.send(response.data);
+});
+
+
+// will be called by the frontend app after the url /punchout/:id is opened in browser
+app.post('/punchout/login', async (req, res) => {
 
   // response fields are snake case
   let response = await axios
@@ -35,6 +48,9 @@ app.get('/punchout/login', async (req, res) => {
 });
 
 
+// will be called by frontend app, the request should contain the cart object
+// the cart strucutre is be defined by the shop and communicated to instapunchout for configuration
+// returns the data needed to send back to the procurement system 
 app.post('/punchout/cart', async (req, res) => {
 
   let punchoutId = ''// from jwt or from req.query.punchoutId;
@@ -52,6 +68,33 @@ app.post('/punchout/cart', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+declare type Order;
+
+// will be called by the instapunchout to create the order
+// the order strucutre is be defined by the shop and communicated to instapunchout for configuration
+app.post('/punchout/order', async (req, res) => {
+
+  // authenticate that the request came from us
+  const response = await axios.post('https://punchout.cloud/authorize', {
+    body: { authorization: req.query.authorization },
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  if (response.data.authorized) {
+
+    // TODO:  create order in your system
+
+    let order = createOrder(req.body);
+
+    res.json(order);
+
+  } else {
+
+    throw new Error('Unauthorized');
+
+  }
+
 });
